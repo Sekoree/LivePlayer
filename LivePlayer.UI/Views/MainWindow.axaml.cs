811c223d25bc91374d3e7994b2210bc30a5d79dd
Sekoree <me@sekoree.dev>
2022-05-23 14:13:01 +0200

    Double-click on list item to play

--- LivePlayer.UI/Views/MainWindow.axaml.cs
+++ LivePlayer.UI/Views/MainWindow.axaml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
+using LivePlayer.UI.Models;
 using LivePlayer.UI.ViewModels;
 
 namespace LivePlayer.UI.Views
@@ -20,6 +22,17 @@ namespace LivePlayer.UI.Views
             {
                 _ = Task.Run(() => dContext.AddSongToQueue());
             }
+        }
+
+        private void InputElement_OnDoubleTapped(object? sender, RoutedEventArgs e)
+        {
+            if (DataContext is not MainWindowViewModel dContext) 
+                return;
+            if (sender is not Grid { DataContext: TrackModel track }) 
+                return;
+            
+            dContext.QueueSelectedTrack = track;
+            _ = Task.Run(() => dContext.PlayPause());
         }
     }
 }
